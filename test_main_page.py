74423d6d98e97020d1c3a72e78f9a4ee34377f6d
Ilya B <ilbv@mail.ru>
2021-01-20 21:41:15 +0300

    Added tests for images and main page

--- test_main_page.py
+++ test_main_page.py
@@ -6,15 +6,15 @@ link = 'https://yandex.ru/'
 
 
 class TestSearchFromMainPage:
-    @pytest.mark.xfail
+
     @pytest.mark.smoke
     def test_find_with_search_line(self, browser):
         page = MainPage(browser, link)
         page.open()
         page.should_be_search_line()
-        page.input_text_to_search_line('запрос')  # Запрос, вводимый, в поисковой строке
+        page.input_text_to_search_line('4px Кострома')  # Запрос, вводимый, в поисковой строке
         page.should_be_suggest_list()
         page.go_to_result_in_search_line()
         page.should_be_search_result_ul()
-        page.should_first_count_result_is_text('request.com', 5)
+        page.should_first_count_result_is_text('4px.ru', 1)
         # Текст искомой ссылки, сколько результатов от начала выдачи проверяем на соответствие
